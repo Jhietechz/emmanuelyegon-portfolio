c7e2c6d210e1c6edc9f4447ade5687157b9766bb
Jhietechz <jhietech0@gmail.com>
2025-12-09 08:24:41 +0300

    Links update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className="text-center py-6 bg-gray-900 text-white">
      <span className="align-self-center ms-3 flex gap-5 text-2xl">
               <motion.a
-                href="https://linkedin.com/in/your-link"
+                href="https://www.linkedin.com/in/kibet-emmanuel-9b8371363"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-secondary"
@@ -16,7 +16,7 @@ const Footer = () => (
                 <FaLinkedin />
               </motion.a>
               <motion.a
-                href="https://github.com/your-github"
+                href="https://github.com/Jhietechz"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-secondary"
@@ -26,7 +26,7 @@ const Footer = () => (
                 <FaGithub />
               </motion.a>
               <motion.a
-                href="mailto:your.email@example.com"
+                href="mailto:emmanuelyegon513@gmail.com"
                 className="btn-secondary"
                 whileHover={{ scale: 1.2, color: "#C8A2C8" }}
                 title="Email"
